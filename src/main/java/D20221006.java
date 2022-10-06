dd4b78b470c805952f9ccd6b2e6c94cbdff54d75
Hyegyeong Joo <101082130+Hyegyeong25@users.noreply.github.com>
2022-10-06 19:44:44 +0900

    221006 백준 개 10172

---
+++ src/main/java/D20221006.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * 날짜 : 2022-10-06
+ * 시간 : 오전 9:00
+ */
+
+public class D20221006 {
+    public static void main(String[] args) {
+        String str = "" +
+                "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|" +
+                "";
+        System.out.println(str);
+    }
+}
